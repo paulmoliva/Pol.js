3f9f06fe0f2d989e28a4895c979046dcdc5f4e8c
paulmoliva <paulmoliva@gmail.com>
2016-08-23 20:28:50 -0700

    simple html rendering of national and ohio

--- pol.js
+++ pol.js
@@ -1,39 +1,7 @@
-$( () => {
-  $.ajax(
-    {
-      url: 'http://www.realclearpolitics.com/epolls/json/5491_historical.js',
-      dataType: 'jsonp',
-      success: (data) => {
-        console.log(data);
-      }
-    }
-  );
-}
-);/*
-function return_json(obj) {
-  console.log(obj);
-  let poll = obj.poll;
-  $h4 = $('<h3></h3>');
-  $h4.html(poll.title)
-  $('.polls').prepend($h4);
-  let data = poll.rcp_avg[0];
-  $h4 = $('<h4></h4>');
-  $h4.html(data.date);
-  $('.polls').append($h4);
-  let candidates = data.candidate;
-  for (let i = 0; i < candidates.length; i++){
-    let color;
-    color = (i === 0) ? "blue" : "red";
-    $(`#candidate${i}-name`)[0].innerHTML = (candidates[i].name);
-    $(`#candidate${i}-value`)[0].innerHTML = (candidates[i].value);
-    let el = $(`#candidate${i}-value`)[0];
-    $(el).css({'width': `${candidates[i].value * 2}px`, 'border-bottom-color': `${color}`});
-  }
-};*/
 
-// import JSON from 'JSON';
-
-function make_request(pollID){
+let pollData = {};
+var count = 0;
+function _make_request(pollID, callBack){
   const rcpURL = 'http://www.realclearpolitics.com/epolls/json/';
   const historical = '_historical.js';
   const requestURL = rcpURL + pollID + historical;
@@ -44,40 +12,74 @@ function make_request(pollID){
         url: rcpURL + pollID.toString() + historical,
         dataType: 'jsonp',
         success: (data) => {
-          console.log(data);
+          console.log('success!');
+        },
+        error: () => {
+          console.log(pollData);
+          callBack(pollData);
         }
-      }
-    );
-  }
-);
+      });
+  });
 }
 
 function return_json(obj) {
-  $('polls').children().each((i, el) => {
-    el.empty();
-  });
-  let h3 = $('.polls h3');
-  let h4 = $('.polls h4');
-  if (h3.length && h4.length){
-    h3[0].innerHTML = ('');
-    h4[0].innerHTML = ('');
-  }
-  console.log(obj);
+  pollData = obj;
+}
+
+function htmlDisplay(obj){
   let poll = obj.poll;
-  let $h4 = $('<h3></h3>');
-  $h4.html(poll.title)
-  $('.polls').prepend($h4);
+
+  //_emptyTags();
+
+  let $h3 = $('<h3></h3>');
+  $h3.html(poll.title);
+  const $root = $('#polls-root');
+  $root.append(`<div id = poll${count}></div>`);
+  let $pollDiv = $(`#poll${count}`);
+  $pollDiv.prepend($h3);
   let data = poll.rcp_avg[0];
-  $h4 = $('<h4></h4>');
+  let $h4 = $('<h4></h4>');
   $h4.html(data.date);
-  $('.polls').append($h4);
+  $pollDiv.append($h4);
   let candidates = data.candidate;
   for (let i = 0; i < candidates.length; i++){
     let color;
-    color = (i === 0) ? "blue" : "red";
-    $(`#candidate${i}-name`)[0].innerHTML = (candidates[i].name);
-    $(`#candidate${i}-value`)[0].innerHTML = (candidates[i].value);
-    let el = $(`#candidate${i}-value`)[0];
-    $(el).css({'width': `${candidates[i].value * 2}px`, 'border-bottom-color': `${color}`});
+    color = (candidates[i].affiliation === "Democrat") ? "blue" : "red";
+    $pollDiv.append(`<div class = 'candidate-name' id = poll-${count}-candidate${i}-name></div>`);
+    $pollDiv.append(`<div class = 'candidate-value' id = poll-${count}-candidate${i}-value></div>`);
+    $(`#poll-${count}-candidate${i}-name`)[0].innerHTML = (candidates[i].name);
+    $(`#poll-${count}-candidate${i}-value`)[0].innerHTML = (candidates[i].value);
+    let el = $(`#poll-${count}-candidate${i}-value`)[0];
+    $(el).css({'width': `${candidates[i].value * 2}px`, 'border-bottom': 'solid', 'border-bottom-color': `${color}`});
   }
-};
+  count ++;
+}
+
+function _emptyTags(){
+  $('#polls-root').children().each((i, el) => {
+    $(el).remove();
+  });
+}
+
+function asJSON(obj){
+  document.getElementById('polls-root').innerHTML = JSON.stringify(obj);
+}
+
+function presidentialPoll(state = 'national', format = 'html'){
+  let id = _getPollID(state);
+  if (format === 'html'){
+    _make_request(id, htmlDisplay);
+  } else {
+    _make_request(id, asJSON);
+  }
+}
+
+function _getPollID(state){
+  if (state === 'national'){
+    return 5491;
+  } else if (state === 'ohio'){
+    return 5634;
+  } else {
+    return 5491;
+  }
+}
